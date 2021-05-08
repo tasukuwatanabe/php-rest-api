fbc2039e31d19043801c6fdf60505a83b10bdc28
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2021-05-09 06:07:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# php-rest-api
+
+このリポジトリ内のコンテンツは、Youtubeチャンネル[「Travesy Media」](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA)さんの以下の動画を参考に実装したものです。
+
+- [PHP REST API From Scratch [1] - Database & Read](https://youtu.be/OEWXbpUMODk)
+- [PHP REST API From Scratch [2] - Single & Create](https://youtu.be/-nq4UbD0NT8)
+- [PHP REST API From Scratch [3] - Update & Delete](https://youtu.be/tG2U18EmIu4)
---
+++ models/Category.php
@@ -0,0 +1,116 @@
+<?php
+class Category {
+  private $conn;
+  private $table = 'categories';
+
+  public $id;
+  public $name;
+  public $created_at;
+
+  public function __construct($db) {
+    $this->conn = $db;
+  }
+
+  public function read() {
+    $query = 'SELECT
+          id,
+          name,
+          created_at
+        FROM
+          ' . $this->table . '
+        ORDER BY
+          created_at DESC';
+    
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->execute();
+
+    return $stmt;
+  }
+
+  public function read_single() {
+    $query = 'SELECT
+        id,
+        name
+      FROM
+        ' . $this->table . ' p
+      WHERE id = ?
+      LIMIT 0,1';
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1, $this->id);
+
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $this->name = $row['name'];
+  }
+
+  public function create() {
+    $query = 'INSERT INTO ' .
+        $this->table . '
+      SET
+        name = :name';
+
+    $stmt = $this->conn->prepare($query);
+
+    $this->name = htmlspecialchars(strip_tags($this->name));
+
+    $stmt->bindParam(':name', $this->name);
+
+    if($stmt->execute()) {
+      return true;
+    }
+
+    printf('Error: %s.\n', $stmt->error);
+
+    return false;
+  }
+
+  public function update() {
+    $query = 'UPDATE ' .
+        $this->table . '
+      SET
+        name = :name
+      WHERE
+        id = :id';
+
+    $stmt = $this->conn->prepare($query);
+
+    $this->id = htmlspecialchars(strip_tags($this->id));
+    $this->name = htmlspecialchars(strip_tags($this->name));
+
+    $stmt->bindParam(':id', $this->id);
+    $stmt->bindParam(':name', $this->name);
+
+    if($stmt->execute()) {
+      return true;
+    }
+
+    printf('Error: %s.\n', $stmt->error);
+
+    return false;
+  }
+
+  public function delete() {
+    $query = 'DELETE FROM ' .
+      $this->table . '
+      WHERE
+        id = :id';
+
+    $stmt = $this->conn->prepare($query);
+
+    $this->name = htmlspecialchars(strip_tags($this->id));
+
+    $stmt->bindParam(':id', $this->id);
+
+    if($stmt->execute()) {
+      return true;
+    }
+
+    printf("Error: %s.\n", $stmt->error);
+
+    return false;
+  }
+}
